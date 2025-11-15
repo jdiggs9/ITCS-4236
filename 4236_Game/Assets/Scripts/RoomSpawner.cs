e8959b980121d7af1a11ea2fa74c2a14a49e8d7c
jdiggs9 <160246790+jdiggs9@users.noreply.github.com>
2025-11-15 00:37:16 -0500

    Setup for room generation

---
+++ 4236_Game/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+
+    public int openDoor;
+    // 1 = needs a door facing South
+    // 2 = needs a door facing West
+    // 3 = needs a door facing North
+    // 4 = needs a door facing East
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(openDoor == 1) {
+
+        } else if (openDoor == 2) {
+
+        } else if (openDoor == 3) {
+
+        } else if (openDoor == 4) {
+
+        }
+    }
+}
---
+++ 4236_Game/Assets/Scripts/RoomTemplate.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class RoomTemplate : MonoBehaviour
+{
+
+    public GameObject[] NorthRooms;
+    public GameObject[] EastRooms;
+    public GameObject[] SouthRooms;
+    public GameObject[] WestRooms;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
