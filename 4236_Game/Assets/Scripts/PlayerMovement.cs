3dfb2be24fbe24dbac82b3d7ff286d9e986f8d63
Josh Diggs <jdiggs9@uncc.edu>
2025-12-10 10:43:33 -0500

    Items

--- 4236_Game/Assets/Scripts/PlayerMovement.cs
+++ 4236_Game/Assets/Scripts/PlayerMovement.cs
@@ -10,41 +10,44 @@ public class Player_Movement : MonoBehaviour
     private Rigidbody2D playerRB;
     //public Rigidbody2D camRB;
 
-    //private float sprint;
+    private float sprint;
     private Vector2 moveDirection;
     private float horizontal;
     private float vertical;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        //sprint = 1f;
+        sprint = 1f;
         playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
         inputControl();
         //cam.transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
     }
 
     private void inputControl() {
 
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        //horizontal = Input.GetAxisRaw("Horizontal");
+        //vertical = Input.GetAxisRaw("Vertical");
 
-        playerRB.linearVelocity = new Vector2(horizontal * moveSpeed * Time.deltaTime, vertical * moveSpeed * Time.deltaTime);
+        //playerRB.linearVelocity = new Vector2(horizontal * moveSpeed * Time.deltaTime, vertical * moveSpeed * Time.deltaTime);
 
         //input
-        //float moveX = Input.GetAxisRaw("Horizontal");
-        //float moveY = Input.GetAxisRaw("Vertical");
-        //if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
-        //    sprint = 2f;
-        //} else {
-        //    sprint = 1f;
-        //}
-        ////player movement
-        //moveDirection = new Vector2(moveX, moveY).normalized;
-        //playerRB.linearVelocity = moveDirection * 100f * moveSpeed * Time.deltaTime * sprint;
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            sprint = 2f;
+        }
+        else
+        {
+            sprint = 1f;
+        }
+        //player movement
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        playerRB.linearVelocity = moveDirection * 100f * moveSpeed * Time.deltaTime * sprint;
 
         //cam movement
         //camRB.linearVelocity = moveDirection * 100f * moveSpeed * Time.deltaTime * sprint;
